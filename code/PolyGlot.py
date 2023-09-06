60fb8d308e9011fbbb6ace3dce51d8b48e8f971f
nikozdev <nikozdev@nikozdev.net>
2023-09-06 16:55:03 -0700

    !code!venv!test!git!deps!libs!pip!package!math!time!tqdm!progress!tui
    [code/PolyGlot.py] > imported and tested tqdm using math and time modules
    [venv/] > added directory for python virtual environment
    [venv/.gitignore] > added git file to ignore venv generated content
    [venv/reqs.txt] > added plain text file to restore venv dependencies

--- code/PolyGlot.py
+++ code/PolyGlot.py
@@ -1,11 +1,35 @@
+# standard
+
 import sys as mSys
+import time as mTime
+import math as mMath
+
+# external
+
+import tqdm as mTqdm
+
+# contents
+
+## datadef
 
 vArgV = mSys.argv
 
 vTestTab = {}
+
+## actions
+
 def fHello():
     print("HelloWorld")
 vTestTab["Hello"] = fHello
+
+def fTqdm():
+    vRangeFull = 100
+    vRangeHalf = vRangeFull / 2
+    for vIter in mTqdm.tqdm(range(vRangeFull)):
+        mTime.sleep(abs(mMath.sqrt(vIter) - vRangeHalf) * 0.00025)
+vTestTab["Tqdm"] = fTqdm
+
+# finalize
 
 if __name__ == "__main__":
     if (vArgV[1] == "test") if (len(vArgV) > 1) else False:
