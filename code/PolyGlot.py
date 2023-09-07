59524e9b7295fc812fb9682d8cfa035ef52f6475
nikozdev <nikozdev@nikozdev.net>
2023-09-06 17:15:32 -0700

    !code!test!math!plot!lib!random!deps!packages!pip!install!slice
    [code/PolyGlot.py]
    > imported random, numpy, matplotlib
    > speeded up tqdm test
    > added matplotlib linear test

--- code/PolyGlot.py
+++ code/PolyGlot.py
@@ -2,11 +2,18 @@
 
 import sys as mSys
 import time as mTime
+
 import math as mMath
+import random as mRand
 
 # external
 
 import tqdm as mTqdm
+
+import matplotlib as mMpl
+import matplotlib.pyplot as mMplPlot
+
+import numpy as mNumpy
 
 # contents
 
@@ -26,8 +33,19 @@ def fTqdm():
     vRangeFull = 100
     vRangeHalf = vRangeFull / 2
     for vIter in mTqdm.tqdm(range(vRangeFull)):
-        mTime.sleep(abs(mMath.sqrt(vIter) - vRangeHalf) * 0.00025)
+        mTime.sleep(abs(mMath.sqrt(vIter) - vRangeHalf) * 0.0001)
 vTestTab["Tqdm"] = fTqdm
+
+def fMplPlotLinear():
+    vFigure = mMplPlot.figure()
+    vX = [ 0, 1, 2, 3, 4 ]
+    vY = vX[::]
+    mRand.shuffle(vY)
+    print(f"vX={vX}\nvY={vY}")
+    vSuplot = vFigure.add_subplot()
+    vSuplot.plot(vX, vY)
+    mMplPlot.show()
+vTestTab["MplPlotLinear"] = fMplPlotLinear
 
 # finalize
 
